891b11a254a212a6b6a4c227a0664a7a585bfcd1
sagarthapliyal <sagarthapliyal9720@gmail.com>
2025-11-29 13:22:04 +0530

    change

--- HotelBooking/HotelBooking/settings.py
+++ HotelBooking/HotelBooking/settings.py
@@ -187,10 +187,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ncertwale781@gmail.com'
-EMAIL_HOST_PASSWORD = "xnds ygjk fvcu qntj"
